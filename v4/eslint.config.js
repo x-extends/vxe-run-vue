452d29ccf5f12d177395186d01f4a223d1445241
xuliangzhan <xu_liangzhan@163.com>
2026-09-06 18:57:40 +0800

    update docs

---
+++ v4/eslint.config.js
@@ -0,0 +1,76 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+import json from '@eslint/json'
+import markdown from '@eslint/markdown'
+import css from '@eslint/css'
+import eslintConfigFlat from 'eslint-config-flat'
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig([
+  eslintConfigFlat.configs.recommended,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,vue}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
+  {
+    ignores: ['**/node_modules/', '.git/']
+  },
+  tseslint.configs.recommended,
+  pluginVue.configs['flat/recommended'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    }
+  },
+  {
+    files: ['**/*.json'],
+    plugins: { json },
+    language: 'json/json',
+    extends: ['json/recommended']
+  },
+  {
+    files: ['**/*.jsonc'],
+    plugins: { json },
+    language: 'json/jsonc',
+    extends: ['json/recommended']
+  },
+  {
+    files: ['**/*.md'],
+    plugins: { markdown },
+    language: 'markdown/commonmark',
+    extends: ['markdown/recommended']
+  },
+  {
+    files: ['**/*.css'],
+    plugins: { css },
+    language: 'css/css',
+    extends: ['css/recommended']
+  },
+  {
+    rules: {
+      'no-debugger': 'error',
+      'vue/no-v-model-argument': 'off',
+      'vue/multi-word-component-names': 'off',
+      'vue/max-attributes-per-line': 'off',
+      'vue/singleline-html-element-content-newline': 'off',
+      'vue/html-self-closing': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn'
+    }
+  }
+])
--- v4/src/common/ui.ts
+++ v4/src/common/ui.ts
@@ -1,7 +1,7 @@
 import { reactive } from 'vue'
 
-export function useUIStore (events?: any) {
-  const uiVersionList = ['4.18.1']
+export function useUIStore(events?: any) {
+  const uiVersionList = ['4.18.1', '4.17.30']
   const uiVersionOptions = uiVersionList.map(v => ({ label: v, value: v }))
   const uiRender = reactive({
     name: 'VxeSelect',
