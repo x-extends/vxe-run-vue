240e1672f076cb971ca85b9347cb0745a3f1d891
xuliangzhan <xu_liangzhan@163.com>
2026-09-06 20:19:28 +0800

    update docs

--- v4/src/common/ui.ts
+++ v4/src/common/ui.ts
@@ -1,7 +1,7 @@
 import { reactive } from 'vue'
 
 export function useUIStore (events?: any) {
-  const uiVersionList = ['4.18.2', '4.18.1', '4.17.30']
+  const uiVersionList = ['4.18.2', '4.17.30', '4.16.28', '4.15.28']
   const uiVersionOptions = uiVersionList.map(v => ({ label: v, value: v }))
   const uiRender = reactive({
     name: 'VxeSelect',
